2867263950614e71ad2d395b4e10887017419d71
Zhenyuan Acharya <yuanxian.philosophy@gmail.com>
2026-05-21 10:08:54 +0800

    Create Basic.lean

---
+++ formalization/Lean4/Hodge/Basic.lean
@@ -0,0 +1,12 @@
+import YXTT2.0.Manifolds.T64
+
+namespace YXT.Millennium.Hodge
+
+/-- Hodge Class on T⁶⁴ -/
+structure HodgeClass (p : ℕ) where
+  representative : Type _
+
+/-- Hodge Laplacian on T⁶⁴ -/
+def HodgeLaplacianOn (M : Type _) (p : ℕ) : Type _ := sorry
+
+end YXT.Millennium.Hodge
